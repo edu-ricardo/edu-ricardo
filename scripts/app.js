bf37e2e5f030f46d7b49977ace6c649169d2c780
Eduardo <edu_ricardo@live.com>
2015-12-16 20:28:57 -0200

    AngularJS + Jekyll

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,11 @@
-app = angular.module('portifolio',[]);
+var app = angular.module('portifolio',[]);
 
-app.controller('CtrlMain', ['$scope', function($scope){
+app.controller('CtrlMain', function($scope, $http){
 	$scope.teste = 10;
-}]);
+	$http.get('/posts.json')
+		.then(function(response) {
+			console.log(response.data.posts);
+			$scope.posts = response.data.posts;
+		});
+});
 	
